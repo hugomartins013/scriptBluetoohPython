c1ea8a74dac9101d71989ca88596a2c304a46297
HugoMartins <hugomartins_15@hotmail.com>
2016-11-30 00:30:31 -0200

    Código rodando sem erros, porém ainda não está abrindo o socket

--- search.py
+++ search.py
@@ -1,31 +1,25 @@
-
-from bluetooth import *
+import subprocess, bluetooth
 
 print "performing inquiry..."
 
-nearby_devices = discover_devices(lookup_names = True) # Procura todos dispositivos próximos
+nearby_devices = bluetooth.discover_devices(duration=4, lookup_names=True, flush_cache=True) 
 
-print "found %d devices" % len(nearby_devices) # Informa quantos dispositivos foram encontrados
+print "found %d devices" % len(nearby_devices) 
 
-for name, addr in nearby_devices: # Informa os dispositivos encontrados
+for name, addr in nearby_devices: 
      print " %s - %s" % (addr, name)
+ 
+passkey = "1111" 
 
-    name = name      # Nome do dispositivo
-    addr = addr      # Endereço do dispositivo
-    port = bluetooth.get_available_port (bluetooth.RFCOMM) #  Porta RFCOMM (.get_available_port pega a primeira disponível)
-    passkey = "1111" # passkey of the device you want to connect
+subprocess.call("kill -9 `pidof bluetooth-agent`",shell = True)
 
-    # Interrompe outros processos de conexões bluetooth em execução
-    subprocess.call("kill -9 `pidof bluetooth-agent`",shell=True)
+status = subprocess.call("bluetooth-agent " + passkey + " & ", shell = True)
+try:
+    s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    s.connect((addr, 1))
 
-    # Nova conexão bluetooth
-    status = subprocess.call("bluetooth-agent " + passkey + " &",shell=True)
-    try:
-        s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        s.connect((addr[0,port[0]))
-    except bluetooth.btcommon.BluetoothError as err:
-        pass
+except bluetooth.btcommon.BluetoothError as err:
+    pass
 
 s.recv(1024)
 s.send("Hello World!")
-
